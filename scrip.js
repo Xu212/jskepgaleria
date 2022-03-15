6985feeb45873f7488af1a49b9f25ca8a9468819
Xu Jiyu <matexu97@gmail.com>
2022-03-15 20:16:19 +0100

    galéria

---
+++ scrip.js
@@ -0,0 +1,33 @@
+var kepek = ["kepek/_DSC7025.jpg", "kepek/_DSC7365.jpg", "kepek/_DSC7371_1.jpg", "kepek/_DSC7444.jpg", "kepek/_DSC7515.jpg"];
+window.addEventListener("load", init);
+var i = 0;
+function init(){
+    document.getElementById("bal").addEventListener("click", balkep);
+    document.getElementById("jobb").addEventListener("click", jobbkep);
+    document.getElementById("uj").addEventListener("click", randomkep);
+    txt="";
+    for (let index = 0; index < kepek.length; index++) {
+        txt +="<img src='"+kepek[index]+"' alt''>"
+    }
+    document.getElementById("osszes").innerHTML=txt;    
+}
+
+function balkep(){
+    --i;
+    if(i === -1){
+        i=kepek.length-1
+    }
+    document.getElementById("nagykep").src=kepek[i];
+}
+function jobbkep(){
+    ++i;
+    if(i>kepek.length){
+        i=0
+    }
+    document.getElementById("nagykep").src=kepek[i];
+}
+
+function randomkep(){
+    var rnd = Math.floor(Math.random()*kepek.length)
+    document.getElementById("nagykep").src=kepek[rnd];
+}
